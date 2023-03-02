2c98d047138ed1d80c97fe5921a570264320ed53
Wallison da Silva Gregorio <wallisongregorio2022@gmail.com>
2023-03-02 16:32:40 -0300

    autenticação de usuario finalizada

--- backend/controllers/UserControler.js
+++ backend/controllers/UserControler.js
@@ -15,7 +15,39 @@ const generateToken = (id) => {
 
 //register user and sign in 
 const register = async (req, res) => {
-    res.send("resgistro")
+
+    const {name, email, password} = req.body
+
+    //checked user
+    const user = await User.findOne({email}) //buscando email ja cadastrado
+
+    if (user){
+        res.status(422).json({errors: ["email ja cadastrado, utilize outro email !"]})
+        return
+    }
+
+    //generate password hash 
+    const salt = await bcrypt.genSalt() //gerando valor aleatoria
+
+    const passwordHash = await bcrypt.hash(password, salt) //encriptando a senha com o valor aleatorio
+
+    //create user
+    const newUser = await User.create({
+        name,
+        email, 
+        password: passwordHash
+    })
+
+    //consfirmação do usuario e gerando o token 
+    if (!newUser){
+        res.status(422).json({errors: ["Houve um erro, por favor tente mais tarde"]})
+        return
+    }
+
+    res.status(201).json({
+        _id: newUser._id,
+        token: generateToken(newUser._id),
+    });
 };
 
 module.exports = {
--- backend/middlewares/handleValidation.js
+++ backend/middlewares/handleValidation.js
@@ -3,20 +3,20 @@ const {validationResult} = require("express-validator");
 const validate = (req, res, next) => {
 
     //pegango os erros da requisição 
-    const erros = validationResult(req);
+    const errors = validationResult(req);
 
     //verificando se ah algum erro 
-    if(erros.isEmpty()){
+    if(errors.isEmpty()){
         return next();
     }
 
     //extraindo o erro 
     const extractedErros = []
 
-    erros.array().map((err) => extractedErros.push(err.msg))
+    errors.array().map((err) => extractedErros.push(err.msg))
 
     return res.status(422).json({
-        erros: extractedErros
+        errors: extractedErros
     })
 }
 
